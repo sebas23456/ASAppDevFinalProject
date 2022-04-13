fe43ff37075166cc660a5a5008bd696861abc793
Sebas23456 <smarinescuny@gmail.com>
2022-04-13 09:34:09 -0400

    I added stuff

--- app/src/main/java/com/example/asappdevfinalproject/MainActivity.java
+++ app/src/main/java/com/example/asappdevfinalproject/MainActivity.java
@@ -2,15 +2,35 @@ package com.example.asappdevfinalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button1;
+
+
+    public void openActivitySecond() {
+        Intent intent = new Intent(this. ActivitySecond.class);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button1 = (Button) findViewById(R.id.button);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivitySecond();
+            }
+        });
+
     }
+
 
 
 }
