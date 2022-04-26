75027997dbaa2c62a6f85b414059dbbde9697fc7
Sebas23456 <smarinescuny@gmail.com>
2022-04-26 09:09:18 -0400

    I changed some stuff

--- app/src/main/java/com/example/asappdevfinalproject/MainActivity.java
+++ app/src/main/java/com/example/asappdevfinalproject/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.asappdevfinalproject;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,6 +20,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView titleText = findViewById(R.id.textViewTitle);
+        titleText.setTextColor(Color.BLACK);
+
+        TextView usernameText = findViewById(R.id.textViewUsername);
+        usernameText.setTextColor(Color.BLACK);
+
+        TextView passwordText = findViewById(R.id.textViewPassword);
+        passwordText.setTextColor(Color.BLACK);
 
         Button button1 = (Button) findViewById(R.id.button);
         button1.setOnClickListener(view -> openActivitySecond());
