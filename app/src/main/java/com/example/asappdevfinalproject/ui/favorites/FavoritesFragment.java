8cc23435a3bf727d5204a8f79aee47baa3e36324
Sebas23456 <smarinescuny@gmail.com>
2022-06-01 09:22:39 -0400

    Why did all of this happen

--- app/src/main/java/com/example/asappdevfinalproject/ui/favorites/FavoritesFragment.java
+++ app/src/main/java/com/example/asappdevfinalproject/ui/favorites/FavoritesFragment.java
@@ -1,46 +1,66 @@
 package com.example.asappdevfinalproject.ui.favorites;
 
 import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.example.asappdevfinalproject.R;
-import com.example.asappdevfinalproject.databinding.FragmentHomeBinding;
-import com.example.asappdevfinalproject.ui.settings.SettingsViewModel;
 
-public class FavoritesFragment extends Fragment{
-    private SettingsViewModel settingsViewModel;
-    private FragmentHomeBinding binding;
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link FavoritesFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class FavoritesFragment extends Fragment {
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        settingsViewModel =
-                new ViewModelProvider(this).get(SettingsViewModel.class);
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
 
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
 
-        final TextView textView = binding.textHome;
-        settingsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
+    public FavoritesFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment FavoritesFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static FavoritesFragment newInstance(String param1, String param2) {
+        FavoritesFragment fragment = new FavoritesFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
     }
 
     @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_favorites, container, false);
     }
 }
